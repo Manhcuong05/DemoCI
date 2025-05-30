aa444a13cdc53f4c2a3035a83285330f79af5f00
Cường Nguyễn <149052047+Manhcuong05@users.noreply.github.com>
2025-05-30 20:41:21 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("CI test with Jenkins!");
+        System.out.println("CI test with Jenkins - update lần 1!");
     }
 }
